754a23e040ae30c7c500925c0333d42f6137011d
Hunter <eugyneehunter@gmail.com>
2025-04-16 07:23:36 +0300

    Refactor MainContent component for improved readability and consistency

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,15 +4,15 @@ import { Moon, Plus, Sun } from 'lucide-react';
 const MainContent = ({ isDarkMode, setIsDarkMode }) => {
   return (
     <div className="flex-1 transition-all duration-300 bg-[#F3F5F7] px-3 h-full overflow-hidden">
-      <div className={`bg-white border-2 rounded-md border-[rgba(0,0,0,0.08)] h-full p-6 shadow-sm flex flex-col items-center justify-center
+      <div className={`border-2 rounded-md border-[rgba(0,0,0,0.08)] h-full p-6 shadow-sm flex flex-col items-center justify-center
       ${isDarkMode ? 'bg-gray-700 text-white border-[#444]' : 'bg-white text-black border-[rgba(0,0,0,0.08)]'}    
         `}>
         {/* Custom folder SVG */}
         {
 
 
-          isDarkMode ? <Sun size={30} className='cursor-pointer' onClick={() => setIsDarkMode(false)} />
-            : <Moon size={30} className='cursor-pointer' onClick={() => setIsDarkMode(true)} />
+          isDarkMode ? <Sun size={30} className={`cursor-pointer ${isDarkMode ? 'text-white' : 'text-black'} `} onClick={() => setIsDarkMode(false)} />
+            : <Moon size={30} className={`cursor-pointer ${isDarkMode ? 'text-white' : 'text-black'} `} onClick={() => setIsDarkMode(true)} />
         }
         {/* Text */}
         <p className="mt-4 text-sm text-gray-500">
